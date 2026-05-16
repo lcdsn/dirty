46037de590a7c59a50d42edc5e4035d28e2c2312
lucas-caique <lucascaique.nogueira@gmail.com>
2026-05-16 12:24:34 -0300

    feat: deploy to s3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,8 @@
 function Footer() {
     return (
         <footer className="footer">
-            <div className="footer__left">
-                <span className="logo__text-main">OBSCN</span>
-            </div>
             <div className="footer__right">
-                <span className="footer__text">Streetwear for the dirty</span>
+                <span className="footer__text">Streetwear for the DIRTY</span>
                 <span className="footer__text footer__text_year">&copy; {new Date().getFullYear()}</span>
             </div>
         </footer>
